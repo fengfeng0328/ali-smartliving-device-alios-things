14fec18174fe8205448e09830d91fcfa3bd36bab
chenzf <m18819838082@163.com>
2020-03-14 12:25:32 +0800

    delete Living_SDK/example/*

--- Living_SDK/example/id2_app/id2_app.mk
+++
@@ -1,8 +0,0 @@
-NAME := id2_client
-
-$(NAME)_SOURCES := app_entry.c
-$(NAME)_SOURCES += id2_client_demo.c
-
-$(NAME)_COMPONENTS  := id2 irot alicrypto
-
-GLOBAL_INCLUDES += ./
--- Living_SDK/example/lorawan/linkwan/linkwan.mk
+++
@@ -1,9 +0,0 @@
-NAME := linkwan
-
-GLOBAL_INCLUDES += .
-
-linkwan := 1
-
-$(NAME)_SOURCES := linkwan.c
-
-$(NAME)_COMPONENTS := protocols.lorawan
